d8a6671998930839a51bed150833398e05e7b924
leixiaoning <794377332@qq.com>
2021-03-09 12:28:44 +0800

    mv something useless

--- tools/install_kenlm.sh
+++
@@ -1,8 +0,0 @@
-# reference to https://github.com/kpu/kenlm
-cd tools
-wget -O - https://kheafield.com/code/kenlm.tar.gz | tar xz
-
-mkdir -p kenlm/build
-cd kenlm/build
-cmake ..
-make -j 4
--- tools/install_sclite.sh
+++
@@ -1,9 +0,0 @@
-# reference to https://github.com/usnistgov/SCTK
-cd tools
-git clone https://github.com/usnistgov/SCTK.git
-cd SCTK
-export CXXFLAGS="-std=c++11" && make config
-make all
-make check
-make install
-make doc
